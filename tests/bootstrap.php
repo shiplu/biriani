15f6c7c1e4334dbaeb34871c37fcb7b393a7d5d2
shiplu.net@gmail.com <shiplu.net@gmail.com@3e80ad55-c777-d01f-1158-3b2cd5b07928>
2012-04-02 15:58:43 +0000

    automated tests are added. These tests doesn't necessarily work

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author shiplu
+ */
+// TODO: check include path
+
+require_once '../Biriani.php';
+
+// put your code here
+?>
