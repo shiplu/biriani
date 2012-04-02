3798c2c5127bfa9f09186625db5d5edc573c232e
shiplu.net@gmail.com <shiplu.net@gmail.com@3e80ad55-c777-d01f-1158-3b2cd5b07928>
2012-04-02 15:57:46 +0000

    some example file is added

---
+++ examples/index.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once "Console/Table.php";
+
+$Table  = new Console_Table();
+$Table->setHeaders(array('Script name', 'Execution time in seconds'));
+
+function utime() { 
+    list($usec, $sec) = explode(" ",microtime()); 
+    return ((float)$usec + (float)$sec);
+}
+
+function sort_by_modification($a, $b){
+    return filemtime($a)-filemtime($b);
+}
+
+// get all the php files in current directory
+$files = glob("*.php");
+
+// sort them by file modification date
+usort($files , 'sort_by_modification');
+
+// get the most recent modified file.
+$taget_file = array_pop($files);
+
+
+// invoke it.
+$___start = utime();
+require_once $taget_file;
+$___end = utime();
+
+$___execution_time = $___end - $___start;
+$Table->addRow(array($taget_file, number_format($___execution_time,3,'.',',')));
+
+echo PHP_EOL, $Table->getTable(), PHP_EOL;
+?>
