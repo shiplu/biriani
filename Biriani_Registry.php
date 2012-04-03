344566df75d036a48c603e4b55b24a4f951806f9
shiplu.net@gmail.com <shiplu.net@gmail.com@3e80ad55-c777-d01f-1158-3b2cd5b07928>
2012-04-03 19:39:39 +0000

    1.Unregistered Non-existing Twitter and wordpress parser

--- Biriani_Registry.php
+++ Biriani_Registry.php
@@ -10,17 +10,30 @@
  * Class Biriani_Registry
  * Registers the available extractable service classes
  */
-class Biriani_Registry{
+class Biriani_Registry {
+
     /**
      * Extractable service classes. Class names should be sorted from most
      * preferable to lest preferable.
      * @var array
      */
     public static $services = array(
-        'FeedBiriani',
-        'TwitterBiriani',
-        'WordpressBiriani'
+        'FeedBiriani' => 'FeedBiriani.php',
+        'HTMLBiriani' => 'HTMLBiriani.php'
     );
-}
 
+    public static function exists($extractable) {
+        return isset(self::$services[$extractable]);
+    }
+
+    public static function getfilename($extractable) {
+        if (self::exists($extractable)) {
+            return self::$services[$extractable];
+        } else {
+            $message = "Extractable '$extractable' is not found in Registry.";
+            throw new BirianiMatchedExtractableNotFoundException($message);
+        }
+    }
+
+}
 ?>
