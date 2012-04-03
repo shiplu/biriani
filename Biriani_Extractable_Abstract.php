f582d94f4c0d1f97b7856900268cad4a4852a3e9
shiplu.net@gmail.com <shiplu.net@gmail.com@3e80ad55-c777-d01f-1158-3b2cd5b07928>
2012-04-03 19:37:17 +0000

    Biriani_Data and DomDocument added so that extractors can easily extract

--- Biriani_Extractable_Abstract.php
+++ Biriani_Extractable_Abstract.php
@@ -9,15 +9,34 @@
 
 /**
  * class ExtractableBiriani
+ * @property Biriani_Response $response HTTP response
+ * @property Biriani_Data $data last extracted data is cached here
+ * @property DOMDocument $dom internal DOM manipulation object. Access is protected
  */
 abstract class Biriani_Extractable_Abstract implements IExtractable {
  
     protected $response;
-    
+    protected $dom;
+    public $data;
     public function __construct(Biriani_Response $resp) {
         if($resp instanceof Biriani_Response){
             $this->response = $resp;
         }
+        $this->data = new Biriani_Data();
+        
+        // setting up DOMDocument
+        $this->dom = new DOMDocument(); 
+        $this->dom->recover = true;
+        
+        // Disabling dom error and enabling libxml internal error.
+        // This is because if any invalid markup found DOM shows 
+        // a lot of warning.
+        libxml_use_internal_errors(true);
+    }
+    
+    public function __destruct(){
+        // Reverting back the internal errors flag.
+        libxml_use_internal_errors(false);
     }
 }
 ?>
