d5c823a18e9d9211e1f849265cad302a82543efe
Shiplu Mokaddim <shiplu.net@gmail.com>
2012-12-20 10:15:27 +0600

    Added new layout change of twitter.com

--- recipes/TwitterBiriani.php
+++ recipes/TwitterBiriani.php
@@ -14,45 +14,48 @@ class TwitterBiriani extends Biriani_Extractable_Abstract {
     public function extract() {
         $this->load_dom_html();
 
-        /** @var $status DOMNode */
-        $status = $this->dom
-                        ->getElementById("timeline")
-                        ->getElementsByTagName("li")->item(0)
-                        ->getElementsByTagName("span")->item(0);
-        // $status contains now first 'span.status-body' 
+        $domxpath = new DOMXPath($this->dom);
 
-        $status_body = trim($status
-                        ->getElementsByTagName("span")
-                        ->item(0)->textContent);
+        $tweets =$domxpath->query("//div[@class=\"content\"]"); 
+        $tweet =  $tweets->item(0);
 
-        $link = trim($status
-                        ->getElementsByTagName("a")->item(1)
-                ->attributes->getNamedItem('href')
-                ->nodeValue);
+        $statuses = $domxpath->query("//p[@class=\"js-tweet-text\"]", $tweet);
+        $status = $statuses->item(0)->textContent;
 
-        $date = trim((string) $status
-                        ->getElementsByTagName("span")->item(3)
-                ->attributes->getNamedItem('data')
-                ->nodeValue);
+        $links = $domxpath->query("div/small/a", $tweet);
+        $link = $links->item(0);
+        $href = $link->getAttribute('href');
+        
+        if(!preg_match("#^https?://#", $href)){
+            $href=  ltrim($href, '/');
+            $href="http://twitter.com/".$href;
+        }
 
+        $small_tags = $domxpath->query("div/small/a/span", $tweet);
+        $small = $small_tags->item(0);
 
-        preg_match("/time:[\"']([^'\"]+)/", $date, $m);
-
-        $date = strtotime($m[1]);
-
-        return $this->cache_data($status_body, $status_body, $date, $link);
+        $date =  $small->getAttribute("data-time");
+        
+        return $this->cache_data($status, $status, $date, $href);
     }
 
     public static function can_extract(Biriani_Response $response) {
-        $con = $response->get_content();
+        $d = new DOMDocument("1.0", "utf-8");
+        $d->recover = true;
+
+        // Disabling dom error and enabling libxml internal error.
+        // This is because if any invalid markup found DOM shows 
+        // a lot of warning.
+        libxml_use_internal_errors(true);
+
+        $d->loadHTML($response->get_content());
+        $con = $d->getElementsByTagName('body')->item(0)->textContent;
+
         $protected = preg_match("/protected their tweets/i", $con);
-        $screen_name = preg_match(
-                '<meta content="[^"]+" name="page-user-screen_name" />'
-                , $con);
-        $rss_url = preg_match(
-                '|http://twitter.com/statuses/user_timeline/\d+.rss|'
-                , $con);
-        return $screen_name && $rss_url && !$protected;
+        $screen_name = preg_match('/@([\w\d]+)/', $con, $matches1);
+        $stats = preg_match_all('/([0-9,]+)\s+(tweets|followers|following)/i', $con, $matches2);
+
+        return $screen_name && $stats && isset($matches2[1]) && isset($matches2[1][2]) && !$protected;
     }
 
 }
